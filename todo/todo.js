9ae73f7b0116038e8752b85d79ef23d19b4abba8
Marius CA <mariuskcodeacademy@gmail.com>
2023-07-12 11:53:39 +0300

    todo arr

--- todo/todo.js
+++ todo/todo.js
@@ -1,6 +1,13 @@
 'use strict';
 console.log('todo.js file was loaded');
 
+const mainTodosArr = [
+  { id: 1, title: 'Do pushups', isDone: true },
+  { id: 2, title: 'Do laundry', isDone: false },
+  { id: 1, title: 'Go for a walk', isDone: false },
+  { id: 1, title: 'Do pushups', isDone: true },
+];
+
 // sukurti nusitaikymu objekta
 
 // nusitaikyti i forma, ul elementa ir input elementa
@@ -9,10 +16,13 @@ console.log('todo.js file was loaded');
 
 // pateikiant forma paimti ivesties lauko reiksme
 
+// sukuri atskira funkcija
 // sukurti nauja li elementa su reikmes is iviesties lauko
 // patalpinti saraso elemente
 
 // sunkiau
+// gauti esama elementa pagal event.target
+
 // paspaudus ant delete mes trinsim tevini elementa
 
 // paspaudus ant span su tekstu mes toglinsim clase .doneTodo
