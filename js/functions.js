8b60cbc4edbcc16abe9ab8e0caf2b7e6dffec0e5
Marius CA <mariuskcodeacademy@gmail.com>
2023-07-12 09:06:33 +0300

    uzd

--- js/functions.js
+++ js/functions.js
@@ -30,3 +30,36 @@ darkModeOn();
 darkModeOn('blueviolet');
 
 // 2. Returns value
+const num1 = 50;
+const num2 = 150;
+
+const num3 = 30;
+const num4 = 70;
+
+function sum(sk1, sk2) {
+  const rezult = sk1 + sk2;
+  console.log('rezult ===', rezult);
+  return rezult;
+}
+function getArraySum(numArr) {
+  // kad veiktu
+  // atspausdinti ir grazinti suma
+}
+getArraySum([1, 2, 3, 4]);
+getArraySum([1, 2]);
+let gryzoGrazaIsFunkcionsVel = sum(num1, num2);
+let gryzoGrazaIsFunkcionsVel1 = sum(num3, num4);
+console.log('gryzoGrazaIsFunkcionsVel ===', gryzoGrazaIsFunkcionsVel);
+console.log('gryzoGrazaIsFunkcionsVel1 ===', gryzoGrazaIsFunkcionsVel1);
+
+const rezult = gryzoGrazaIsFunkcionsVel + gryzoGrazaIsFunkcionsVel1;
+const rezult1 = sum(gryzoGrazaIsFunkcionsVel, gryzoGrazaIsFunkcionsVel1);
+console.log('rezult ===', rezult);
+console.log('rezult1 ===', rezult1);
+
+// sukurti html elementa 'p'
+// prideti jam klase 'special'
+// prideti jam tekta "i am new el"
+// patalpinti ji div su id 'app'
+
+// padaryti kad tai vyktu su funkcija
