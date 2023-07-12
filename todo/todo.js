4d194cbad3148aa9e60f5299289e707734f97b83
Marius CA <mariuskcodeacademy@gmail.com>
2023-07-12 11:46:23 +0300

    uzd

--- todo/todo.js
+++ todo/todo.js
@@ -1,2 +1,18 @@
 'use strict';
 console.log('todo.js file was loaded');
+
+// sukurti nusitaikymu objekta
+
+// nusitaikyti i forma, ul elementa ir input elementa
+
+// sustabdyti forma nuo perkrovimo
+
+// pateikiant forma paimti ivesties lauko reiksme
+
+// sukurti nauja li elementa su reikmes is iviesties lauko
+// patalpinti saraso elemente
+
+// sunkiau
+// paspaudus ant delete mes trinsim tevini elementa
+
+// paspaudus ant span su tekstu mes toglinsim clase .doneTodo
